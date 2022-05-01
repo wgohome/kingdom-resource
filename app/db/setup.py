8f41965c81f113e27f71d9275a43bcd016e1fe52
William <wirriamm@gmail.com>
2022-05-01 14:02:31 +0800

    add unique index on tax (taxid) of species doc

--- app/db/setup.py
+++ app/db/setup.py
@@ -1,7 +1,16 @@
 from functools import lru_cache
-from pymongo import MongoClient
+from pymongo import ASCENDING, MongoClient
+from app.models.species import SpeciesDoc
 
 from config import settings
+
+
+def setup_indexes(db):
+    db[SpeciesDoc.Mongo.collection_name].create_index(
+        [("tax", ASCENDING)],
+        unique=True,
+        name="unique_taxids"
+    )
 
 
 @lru_cache
@@ -10,6 +19,7 @@ def get_db():
     if settings.DATABASE_NAME is None or settings.DATABASE_NAME == "":
         raise ValueError("DATABASE_NAME env variable missing")
     db = client[settings.DATABASE_NAME]
+    setup_indexes(db)
     return db
     # Returns db instead of yield as we are using lru_cache
     # With yield, a generator will be returned and
